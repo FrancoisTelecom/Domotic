89a3a069b99452c3f2935319af7dc2de463e1c4d
FrancoisTelecom <francois.5@hotmail.fr>
2017-07-29 17:35:33 +0200

    on essaye de mieux faire

--- dht11.c
+++ dht11.c
@@ -1,24 +1,44 @@
-#include <wiringPi.h>
+//#include <wiringPi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #define MAX_TIME 85
 #define DHT11PIN 7
 int dht11_val[5]={0,0,0,0,0};
+
+void openFile(char* insert){
+  FILE* fichier = NULL;
+  fichier = fopen("/data/git/Domotic/output/test290720171716", "a+");
+  if (fichier != NULL)
+  {
+    fputc(insert,fichier);
+    fclose(fichier);
+  }
+
+    else printf("Impossible d'ouvrir le fichier test.txt");
+}
+fclose(fichier);
+void writeOutput(){
+}
+
+
 void dht11_read_val()
 {
   uint8_t lststate=HIGH;
   uint8_t counter=0;
   uint8_t j=0,i;
   float farenheit;
-  for(i=0;i<5;i++)
-     dht11_val[i]=0;
+  //char chaine[];
+
+  for(i=0;i<5;i++) dht11_val[i]=0; //init 0
+  /*init pin rasp*/
   pinMode(DHT11PIN,OUTPUT);
   digitalWrite(DHT11PIN,LOW);
   delay(18);
   digitalWrite(DHT11PIN,HIGH);
   delayMicroseconds(40);
   pinMode(DHT11PIN,INPUT);
+
   for(i=0;i<MAX_TIME;i++)
   {
     counter=0;
@@ -43,7 +63,9 @@ void dht11_read_val()
   if((j>=40)&&(dht11_val[4]==((dht11_val[0]+dht11_val[1]+dht11_val[2]+dht11_val[3])& 0xFF)))
   {
     farenheit=dht11_val[2]*9./5.+32;
-    printf("H = %d.%d\nT = %d.%d\n",dht11_val[0],dht11_val[1],dht11_val[2],dht11_val[3]);
+    //printf("H = %d.%d\nT = %d.%d\n",dht11_val[0],dht11_val[1],dht11_val[2],dht11_val[3]);
+    char chaine[] = ("H = %d.%d\nT = %d.%d\n",dht11_val[0],dht11_val[1],dht11_val[2],dht11_val[3]);
+    openFile(chaine);
   }
   else
     printf("Invalid Data!!\n");
@@ -53,15 +75,16 @@ void dht11_read_val()
 int main(void)
 {
   int b =0;
+
   printf("Interfacing Temperature and Humidity Sensor (DHT11) With Raspberry Pi\n");
   if(wiringPiSetup()==-1) exit(1);
   while(1) {
-    while ( b < 6 ){
-      printf("H = %d.%d\nT = %d.%d\n",dht11_val[0],dht11_val[1],dht11_val[2],dht11_val[3]);
+    /*while ( b < 6 ){
+      ("H = %d.%d\nT = %d.%d\n",dht11_val[0],dht11_val[1],dht11_val[2],dht11_val[3]);
       printf("digitalRead= %d\n", digitalRead(DHT11PIN));
       delay(1000);
       b++;
-    }
+    }*/
      dht11_read_val();
      delay(3000);
   }
