a10ccac77d501e5c6842635f0b4b51b24b0e89e5
FrancoisTelecom <francois.5@hotmail.fr>
2017-07-30 22:37:12 +0200

    Corr inserset.php v0.1

--- insert.php
+++ insert.php
@@ -1,5 +1,5 @@
 #! /bin/php
-/*exec :php insert.php */
 <?php
-print("Hello World !"); // Je préfère print, mais echo a aussi souvent la cote :)
+//exec :php insert.php
+print("Hello World !\n");
 ?>
