02a338769dfa3d14c680c3f485e62e4293bea898
FrancoisTelecom <francois.5@hotmail.fr>
2017-07-31 08:35:10 +0200

    evol insert pdo v0.1

--- insert.php
+++ insert.php
@@ -1,5 +1,23 @@
 #! /bin/php
+
 <?php
+  private $_bdd
+public function __construct()	{
+		try {
+			$this->_bdd=new PDO('mysql:host=localhost;dbname=station_meteo_local;charset=utf8','pi','r@spi');
+		}
+		catch(Exception $e){
+      die('Erreur : '.$e->getMessage());
+    }
+	}
+
+/*
+$req = $this->_bdd->prepare('SELECT id FROM t_utilisateur WHERE identifiant = :pseudo AND motdepasse = :pass');
+		$req->execute(array(
+    						'pseudo' => $_POST['identifier'],
+    						'pass' => $_POST['pass']));
+
+*/
 //exec :php insert.php
 print("Hello World !\n");
 ?>
